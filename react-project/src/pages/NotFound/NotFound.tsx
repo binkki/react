d9a097d74940b2dfbf75a2e75d6570665a64eff6
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-07-28 22:25:11 +0400

    fix: fix redirect from 404 page

--- react-project/src/pages/NotFound/NotFound.tsx
+++ react-project/src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { useDispatch } from 'react-redux';
 import { ThemeContext } from '../../context/ThemeContext';
 import { getCurrentTheme } from '../../utils/utils';
 import { IMAGE404 } from '../../utils/constants';
 import './NotFound.css';
+import { setPage } from '../../store/slices/appSlice';
 
 export default function NotFoundPage() {
   const darkTheme = useContext(ThemeContext);
+  const dispatch = useDispatch();
+
+  const updatePage = () => {
+    dispatch(setPage(1));
+  };
 
   return (
     <div
@@ -14,7 +21,7 @@ export default function NotFoundPage() {
       data-testid="not-found"
     >
       <img src={IMAGE404} alt="not found" className="cart-empty" />
-      <Link to="/" className="error-link">
+      <Link to="/" className="error-link" onClick={updatePage}>
         GO HOME
       </Link>
     </div>
